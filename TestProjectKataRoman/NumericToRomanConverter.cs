6e9940c132ffce8b35c6e9764d5c71b0b415309c
vgrieu <v.grieu@gmail.com>
2023-03-29 14:12:23 +0200

    Add project files.

---
+++ TestProjectKataRoman/NumericToRomanConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace TestProjectKataRoman
+{
+    internal class NumericToRomanConverter
+    {
+        public NumericToRomanConverter()
+        {
+        }
+
+        internal string NumericToRoman(int normalNumber)
+        {
+            if (normalNumber == 4)
+                return "IV";
+
+            return new String('I', normalNumber);
+        }
+    }
+}
---
+++ TestProjectKataRoman/NumericToRomanConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectKataRoman
+{
+    internal class NumericToRomanConverterTests
+    {
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        public void NumericToRoman_Should_Return_Roman_When_Passing_Normal_Number(int numericInput, string expectedResult)
+        {
+            var converter = new NumericToRomanConverter();
+            string result = converter.NumericToRoman(numericInput);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
