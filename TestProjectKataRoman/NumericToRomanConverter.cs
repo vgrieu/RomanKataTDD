0296d0085e4598c72295e5549f584f7fb2c3d775
vgrieu <v.grieu@gmail.com>
2023-03-30 14:40:33 +0200

    Reaching XXXX

--- TestProjectKataRoman/NumericToRomanConverter.cs
+++ TestProjectKataRoman/NumericToRomanConverter.cs
@@ -2,16 +2,36 @@
 {
     internal class NumericToRomanConverter
     {
+        private const char ROMAN_NUMBER_ONE = 'I';
+        private const string ROMAN_NUMBER_FIVE = "V";
+        private const string ROMAN_NUMBER_TEN = "X";
+        private const string ROMAN_NUMBER_FIFTY = "L";
+
         public NumericToRomanConverter()
-        {
-        }
+        { }
 
         internal string NumericToRoman(int normalNumber)
         {
-            if (normalNumber == 4)
-                return "IV";
+            int threshold = 40;
+            string roman_threshold = ROMAN_NUMBER_TEN + ROMAN_NUMBER_FIFTY;
 
-            return new String('I', normalNumber);
+            if (normalNumber >= threshold)
+                return roman_threshold + NumericToRoman(normalNumber - threshold);
+
+            threshold = 10;
+            roman_threshold = ROMAN_NUMBER_TEN;
+
+            if (normalNumber >= threshold)
+                return roman_threshold + NumericToRoman(normalNumber - threshold);
+
+            if (normalNumber >= 9)
+                return ROMAN_NUMBER_ONE + ROMAN_NUMBER_TEN + NumericToRoman(normalNumber - 9);
+            if (normalNumber >= 5)
+                return ROMAN_NUMBER_FIVE + NumericToRoman(normalNumber - 5);
+            if (normalNumber >= 4)
+                return ROMAN_NUMBER_ONE + ROMAN_NUMBER_FIVE + NumericToRoman(normalNumber - 4);
+
+            return new String(ROMAN_NUMBER_ONE, normalNumber);
         }
     }
 }
--- TestProjectKataRoman/NumericToRomanConverterTests.cs
+++ TestProjectKataRoman/NumericToRomanConverterTests.cs
@@ -12,6 +12,18 @@ namespace TestProjectKataRoman
         [TestCase(2, "II")]
         [TestCase(3, "III")]
         [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        [TestCase(11, "XI")]
+        [TestCase(12, "XII")]
+        [TestCase(14, "XIV")]
+        [TestCase(20, "XX")]
+        [TestCase(31, "XXXI")]
+        [TestCase(40, "XL")]
         public void NumericToRoman_Should_Return_Roman_When_Passing_Normal_Number(int numericInput, string expectedResult)
         {
             var converter = new NumericToRomanConverter();
