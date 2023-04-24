ed29fcbfbae18f4710ef230e370100ae7bce3790
vgrieu <v.grieu@gmail.com>
2023-04-24 11:02:38 +0200

    kata completion !

--- TestProjectKataRoman/NumericToRomanConverter.cs
+++ TestProjectKataRoman/NumericToRomanConverter.cs
@@ -1,4 +1,6 @@
-﻿namespace TestProjectKataRoman
+﻿using System.Text;
+
+namespace TestProjectKataRoman
 {
     internal class NumericToRomanConverter
     {
@@ -6,58 +8,129 @@
         private const string ROMAN_NUMBER_FIVE = "V";
         private const string ROMAN_NUMBER_TEN = "X";
         private const string ROMAN_NUMBER_FIFTY = "L";
+        private const string ROMAN_NUMBER_HUNDRED = "C";
 
-        private static List<(int, string)> _romanNumbersStepsList = new List<(int, string)>
-            {
-                (40, ROMAN_NUMBER_TEN + ROMAN_NUMBER_FIFTY)
-                , (10, ROMAN_NUMBER_TEN)
-                , (9, ROMAN_NUMBER_ONE + ROMAN_NUMBER_TEN)
-                , (5, ROMAN_NUMBER_FIVE)
-                , (4, ROMAN_NUMBER_ONE + ROMAN_NUMBER_FIVE)
-                , (1, ROMAN_NUMBER_ONE.ToString())
-            };
-
-        /*
-        * On prend un nombre à transformer et on le transforme en nombre romain
-        */
-
-        /*
-        * On prend un nombre à transformer 
-        * on gère toutes les étapes de la numérotation romaine
-        * et on renvoie le résultat en romain
-        */
+        private static List<(int arab, string roman)> _romanNumbersCases = new List<(int, string)>
+        {
+            (90, ROMAN_NUMBER_TEN + ROMAN_NUMBER_HUNDRED)
+            , (50, ROMAN_NUMBER_FIFTY)
+            , (40, ROMAN_NUMBER_TEN + ROMAN_NUMBER_FIFTY)
+            , (10, ROMAN_NUMBER_TEN)
+            , (9, ROMAN_NUMBER_ONE + ROMAN_NUMBER_TEN)
+            , (5, ROMAN_NUMBER_FIVE)
+            , (4, ROMAN_NUMBER_ONE + ROMAN_NUMBER_FIVE)
+            , (1, ROMAN_NUMBER_ONE.ToString())
+        };
 
         /*
-        * On itere sur les étapes de la numérotation romaine
-        * Pour chaque étape, tant que le nombre à transformer est supérieur
-        * ou égal à la valeur de cette étape en nombre arabe
-        * on va ajouter la valeur de l'étape en nombre romain à notre résultat
-        * et soustraire la valeur de l'étape du nombre à transformer.
-        * Jusqu'à la dernière étape, puis on ajoute à notre résultat
-        * un nombre de 'I' egal à ce qu'il reste du nombre à transformer
-        */
-
-        /*
-        * On itere sur les étapes de la numérotation romaine
-        * Pour chaque étape, tant que le nombre à transformer est supérieur
-        * ou égal à la valeur de cette étape en nombre arabe
-        * on va ajouter la valeur de l'étape en nombre romain à notre résultat
-        * et soustraire la valeur de l'étape du nombre à transformer.
+        * On itere sur les cas de la numérotation romaine
+        * Pour chaque cas, 
+        *   tant que le résultat doit s'écrire avec la valeur du cas en nombre romain
+        *     on transfère la valeur du cas du nombre à transformer vers le résultat
         */
 
         internal string NumericToRoman(int numberToTransform)
         {
-            string result = string.Empty;
-            foreach ((int arabStepValue, string romanStepValue) in _romanNumbersStepsList)
+            StringBuilder result = new StringBuilder();
+
+            foreach (var romanNumbersCase in _romanNumbersCases)
             {
-                while (numberToTransform >= arabStepValue)
+                while (ResultIsWrittenWithCurrentRomanCase(numberToTransform, romanNumbersCase))
                 {
-                    numberToTransform = numberToTransform - arabStepValue;
-                    result += romanStepValue;
+                    numberToTransform = TransferCaseValueToResult(numberToTransform, result, romanNumbersCase);
                 }
             }
 
-            return result;
+            return result.ToString();
+        }
+
+        private static int TransferCaseValueToResult(int numberToTransform, StringBuilder result, (int arab, string roman) romanNumbersCase)
+        {
+            numberToTransform -= romanNumbersCase.arab;
+            result.Append(romanNumbersCase.roman);
+            return numberToTransform;
+        }
+
+        private static bool ResultIsWrittenWithCurrentRomanCase(int numberToTransform, (int arab, string roman) romanNumbersCase)
+        {
+            return numberToTransform >= romanNumbersCase.arab;
+        }
+    }
+
+    public class Solution
+    {
+        public string IntToRoman(int num)
+        {
+            List<char> list = new List<char>();
+
+            while (num > 0)
+            {
+                switch (num)
+                {
+                    case >= 1000:
+                        list.Add('M');
+                        num -= 1000;
+                        break;
+                    case >= 900:
+                        list.Add('C');
+                        list.Add('M');
+                        num -= 900;
+                        break;
+                    case >= 500:
+                        list.Add('D');
+                        num -= 500;
+                        break;
+                    case >= 400:
+                        list.Add('C');
+                        list.Add('D');
+                        num -= 400;
+                        break;
+                    case >= 100:
+                        list.Add('C');
+                        num -= 100;
+                        break;
+                    case >= 90:
+                        list.Add('X');
+                        list.Add('C');
+                        num -= 90;
+                        break;
+                    case >= 50:
+                        list.Add('L');
+                        num -= 50;
+                        break;
+                    case >= 40:
+                        list.Add('X');
+                        list.Add('L');
+                        num -= 40;
+                        break;
+                    case >= 10:
+                        list.Add('X');
+                        num -= 10;
+                        break;
+                    case >= 9:
+                        list.Add('I');
+                        list.Add('X');
+                        num -= 9;
+                        break;
+                    case >= 5:
+                        list.Add('V');
+                        num -= 5;
+                        break;
+                    case >= 4:
+                        list.Add('I');
+                        list.Add('V');
+                        num -= 4;
+                        break;
+                    case >= 1:
+                        list.Add('I');
+                        num -= 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return string.Concat(list);
         }
     }
 }
--- TestProjectKataRoman/NumericToRomanConverterTests.cs
+++ TestProjectKataRoman/NumericToRomanConverterTests.cs
@@ -24,10 +24,40 @@ namespace TestProjectKataRoman
         [TestCase(20, "XX")]
         [TestCase(31, "XXXI")]
         [TestCase(40, "XL")]
+        [TestCase(50, "L")]
+        [TestCase(60, "LX")]
+        [TestCase(90, "XC")]
         public void NumericToRoman_Should_Return_Roman_When_Passing_Normal_Number(int numericInput, string expectedResult)
         {
             var converter = new NumericToRomanConverter();
             string result = converter.NumericToRoman(numericInput);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        [TestCase(11, "XI")]
+        [TestCase(12, "XII")]
+        [TestCase(14, "XIV")]
+        [TestCase(20, "XX")]
+        [TestCase(31, "XXXI")]
+        [TestCase(40, "XL")]
+        [TestCase(50, "L")]
+        [TestCase(60, "LX")]
+        [TestCase(90, "XC")]
+        public void SolutionToRoman_Should_Return_Roman_When_Passing_Normal_Number(int numericInput, string expectedResult)
+        {
+            var converter = new Solution();
+            string result = converter.IntToRoman(numericInput);
 
             Assert.That(result, Is.EqualTo(expectedResult));
         }
